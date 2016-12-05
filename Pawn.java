5f9fa3d1b6458f50b93b309e98a10528d347a13d
ProjektTP <aleksandra.kuriata96@gmail.com>
2016-12-05 15:21:57 +0100

    Add files via upload

---
+++ Pawn.java
@@ -0,0 +1,15 @@
+package client;
+
+public class Pawn {
+
+	private boolean isAlive;
+
+	boolean isAlive() {
+		return isAlive;
+	}
+
+	void setIsAlive(boolean newIsAlive) {
+		this.isAlive = newIsAlive;
+	}
+
+}
