6aba0f914182701b44e648472f54a9514592918e
ProjektTP <aleksandra.kuriata96@gmail.com>
2016-12-14 18:24:09 +0100

    Delete KoException.java

--- src/supertableanditsexceptions/KoException.java
+++
@@ -1,8 +0,0 @@
-package supertableanditsexceptions;
-
-public class KoException extends Exception 
-{
-	/**
-	 * Illegal move that will create a Ko situation, player has to choose another field
-	 */
-}
