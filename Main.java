8616450b6d76821188f083a2e5957ce4f4141b8c
ProjektTP <aleksandra.kuriata96@gmail.com>
2016-12-21 20:48:55 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			Move move = new Move();
+			move.getTab();
+			move.game();
+		} catch (ArrayException e) {
+			System.out.println(e.getMessage());
+			// break;
+		}
+	}
+}
