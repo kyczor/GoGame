4e4401270010582081f66d2cbdcd06625cab90c7
Ola <ola_kuriata@o2.pl>
2016-12-23 21:27:02 +0100

    Add cornare case validation

--- GoGameIJ/src/Move.java
+++ GoGameIJ/src/Move.java
@@ -41,6 +41,8 @@ public class Move /* implements Functions */ {
 	}
 
 	public void removePlayer(int x, int y, whosefield enemy, whosefield friend) {
+		cornerCase(x, y, enemy, friend);
+
 		if (board.getField(x, y + 1) == enemy) {
 			if (board.getField(x, y) == friend && board.getField(x, y + 2) == friend
 					&& board.getField(x - 1, y + 1) == friend && board.getField(x + 1, y + 1) == friend)
@@ -66,6 +68,52 @@ public class Move /* implements Functions */ {
 			// board.putPawn(whosefield.empty, x - 1, y);
 		}
 	}
+
+	public void cornerCase(int x, int y, whosefield enemy, whosefield friend) {
+		int height = board.tab.get(0).size();
+		int width = board.tab.size();
+
+		if (x == 1 && y == 0) {
+			if (board.getField(0, 0) == enemy && board.getField(0, 1) == friend) {
+				board.tab.get(0).set(0, color.empty);
+			}
+		} else if (x == 0 && y == 1) {
+			if (board.getField(0, 0) == enemy && board.getField(1, 0) == friend) {
+				board.tab.get(0).set(0, color.empty);
+			}
+		}
+
+		if (x == (width - 1) && y == 0) {
+			if (board.getField(width, 0) == enemy && board.getField(width, 1) == friend) {
+				board.tab.get(width).set(0, color.empty);
+			}
+		} else if (x == width && y == 1) {
+			if (board.getField(width, 0) == enemy && board.getField(width - 1, 0) == friend) {
+				board.tab.get(width).set(0, color.empty);
+			}
+		}
+
+		if (x == 0 && y == (height - 1)) {
+			if (board.getField(0, height) == enemy && board.getField(1, height) == friend) {
+				board.tab.get(0).set(height, color.empty);
+			}
+		} else if (x == 1 && y == height) {
+			if (board.getField(0, height) == enemy && board.getField(0, height - 1) == friend) {
+				board.tab.get(0).set(height, color.empty);
+			}
+		}
+
+		if (x == board.tab.size() && y == (board.tab.get(board.tab.size()).size() - 1)) {
+			if (board.tab.get(width).get(height) == enemy && board.tab.get(width - 1).get(height) == friend) {
+				board.tab.get(width).set(height, color.empty);
+			}
+		} else if (x == (board.tab.size() - 1) && y == board.tab.get(0).size()) {
+			if (board.tab.get(width).get(height) == enemy && board.tab.get(width).get(height - 1) == friend) {
+				board.tab.get(width).set(height, color.empty);
+			}
+		}
+	}
+
 }
 //
 // public void move(int row, int column, Integer player) {
